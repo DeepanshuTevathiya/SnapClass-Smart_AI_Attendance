a24e20c166d99bf16a953ed533043972fbd824d2
Deepanshu Tevathiya <deepanshutev@gmail.com>
2026-05-01 16:38:37 +0530

    Teacher_authentication

--- src/Database/db.py
+++ src/Database/db.py
@@ -23,21 +23,3 @@ def teacher_login(username, password):
         if check_pass(password, teacher["password"]):
             return teacher
     return None
-
-
-def check_student_exist(username):
-    response = supabase.table("student").select("username").eq("username",username).execute()
-    return len(response.data)>0
-
-def create_student(username, name, password):
-    data = {"username":username, "name":name, "password":hash_pass(password)}
-    response = supabase.table("student").insert(data).execute()
-    return response.data
-
-def student_login(username, password):
-    response = supabase.table("student").select("*").eq("username",username).execute()
-    if response.data:
-        student = response.data[0]
-        if check_pass(password, student["password"]):
-            return student
-    return None
--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -2,18 +2,14 @@ import streamlit as st
 from src.ui.base_layout import style_background_dashboard, style_base_layout
 from src.components.header import header_dashboard
 from src.components.footer import footer_dashboard
-from src.Database.db import check_student_exist, create_student, student_login
-
+from PIL import Image
+import numpy as np
 
 def student_screen():
 
     style_background_dashboard()
     style_base_layout()
 
-    student_screen_login()
-    
-
-def student_screen_login():
     c1, c2 = st.columns(2, vertical_alignment="center", gap="xxlarge")
     with c1:
         header_dashboard()
@@ -26,7 +22,10 @@ def student_screen_login():
     st.space()
     st.space()
 
-    st.camera_input("Position your face in center")
+    photo_source = st.camera_input("Position your face in center")
+
+    if photo_source:
+        np.array(Image.open(photo_source))
 
     st.divider()
     footer_dashboard()
