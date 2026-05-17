dc0e5cbdf8dd5a0d4ee4766a38927e0eb500ed8a
Deepanshu Tevathiya <deepanshutev@gmail.com>
2026-05-17 20:19:34 +0530

    Final Touches

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,23 +1,26 @@
 import streamlit as st
 
 def footer_home():
-    
-    logo_url = "https://iili.io/BiqQIa9.png"
 
     st.markdown(f"""                
         <div style="margin-top: 2rem; display: flex; gap: 6px; justify-content: center; align-items: center;">
-            <p style='font-weight: bold; color: white; margin: 0;'>Created with ♥️ by team</p>
-            <img src='{logo_url}' style='max-height: 25px;'/>                
+            <p style='font-weight: bold; color: white; margin: 0;'>Created with ♥️ by
+                <span style="font-weight: 800; font-size: 17px; margin-left: 4px">
+                    Deepanshu Tevathiya 
+                </span>
+            </p>
+
         </div>
                 """, unsafe_allow_html=True)
 
 def footer_dashboard():
     
-    logo_url = "https://iili.io/BiqQIa9.png"
-
     st.markdown(f"""                
         <div style="margin-top: 2rem; display: flex; gap: 6px; justify-content: center; align-items: center;">
-            <p style='font-weight: bold; color: black; margin: 0;'>Created with ♥️ by team</p>
-            <img src='{logo_url}' style='max-height: 25px;'/>                
+            <p style='font-weight: bold; color: black; margin: 0;'>Created with ♥️ by
+                <span style="font-weight: 800; font-size: 17px; margin-left: 4px">
+                    Deepanshu Tevathiya 
+                </span>
+            </p>
         </div>
                 """, unsafe_allow_html=True)
