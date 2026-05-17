c4d87c95a63f5eb13f05c112ec3d752c67ac83f9
Deepanshu Tevathiya <deepanshutev@gmail.com>
2026-05-17 20:54:52 +0530

    fix: add .python-version file

--- src/screens/teacher_screen.py
+++ src/screens/teacher_screen.py
@@ -33,7 +33,7 @@ def teacher_dashboard():
     with c1:
         header_dashboard()
     with c2:
-        st.subheader(f"Welcome, {teacher_data["name"]}")
+        st.subheader(f"Welcome, {teacher_data['name']}")
         if st.button("Logout", type="secondary", key="loginbckbtn", shortcut="control+backspace"):
             st.session_state["is_logged_in"] = False
             del st.session_state.teacher_data
